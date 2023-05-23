5db63f887939337ba2ba8d7305e33bdddca2d76b
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-05-23 08:06:52 +0300

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:appointment_app/pages/login_page.dart';
 import 'package:appointment_app/pages/signup_page.dart';
 import 'package:appointment_app/pages/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
